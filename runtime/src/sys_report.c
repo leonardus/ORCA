5651ba89618f6b5d2e3ff2517b662cd6bf0afeb8
leonardus <leonardus@b-division.net>
2024-12-27 17:37:17 -0500

    Add copyright notice

--- runtime/src/sys_report.c
+++ runtime/src/sys_report.c
@@ -34,6 +34,10 @@ distribution.
 ORCA
 Copyright (C) 2024 leonardus
 
+Copyright (C) 2004
+Michael Wiedenbauer (shagkur)
+Dave Murphy (WinterMute)
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
