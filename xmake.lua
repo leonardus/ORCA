3ad88cfa2b55813acecfeb0919bfaee5997c105b
leonardus <leonardus@b-division.net>
2025-01-28 12:08:49 -0500

    Fixes for clangd

--- xmake.lua
+++ xmake.lua
@@ -51,6 +51,7 @@ toolchain("devkitppc")
 	add_cxflags(march)
 	add_asflags(march)
 	add_ldflags(march)
+	add_sysincludedirs(path.join(devkitpro_dir, "devkitPPC", "powerpc-eabi", "include")) -- needed for clangd compile commands
 	add_syslinks("m")
 	add_defines("GEKKO")
 toolchain_end()
